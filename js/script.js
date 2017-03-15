46cdef3b848dea9ac52fbb0c43b2e1753bfdf7cd
fhergomez <fhergomez37@gmail.com>
2017-03-14 23:58:42 -0700

    work css,html, and js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
 
  console.log('jQuery is ready');
 
+ var url = 'https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/';
+ $.ajax ({
+  type: 'GET',
+  url: url,
+  dataType: 'json',
+  headers: {
+    'w4phosouw8u1vbu5jtcm5jdbdpamsm7rdvmz69v8wjuij93w7yh4mjs1modf'
+  },
+  success: function (data) {
+    console.log(data);
+  },
+  error: function (errorMessage) {
+    console.log(errorMessage);
+  }
+ });
+
 });
