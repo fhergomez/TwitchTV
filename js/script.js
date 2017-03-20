61b0b173273ead7e46bda2a266d5dfb0146ba563
fhergomez <fhergomez37@gmail.com>
2017-03-20 00:13:43 -0700

    API working

--- js/script.js
+++ js/script.js
@@ -2,21 +2,66 @@ $(document).ready(function() {
 
  console.log('jQuery is ready');
 
- var url = 'https://api.twitch.tv/kraken/streams/freecodecamp';
- $.ajax ({
-  type: 'GET',
-  url: url,
-  dataType: 'json',
-  headers:{
-    'Client-ID': 'y2v1et51byje4zrtyp0bkjnu4qw3li'
-  },
-  success: function (data) {
-    console.log(data);
-    $('.logo').html('<a href="' + data.follows[0].channel.url + '" target="_blank">' + data.follows[0].channel.name + '</a>');
-  },
-  error: function (errorMessage) {
-    console.log(errorMessage);
-  }
- });
+ var url = 'https://wind-bow.gomix.me/twitch-api/';
+ var streams = 'streams/';
+ var channels = ["freecodecamp", "nintendo", "food", "bobross"];
+ var users = 'users/';
+ var channel;
 
+ function onLoad() {
+   for (var i = 0;i < channels.length;i++) {
+    channel = channels[i];
+    // console.log(channel);
+
+
+     $.ajax ({
+        type: 'GET',
+        headers:{
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        dataType: 'jsonp',
+        url: url + streams + channel,
+        success: function (data1) {
+          stream = data1.stream;
+          console.log(data1);
+          console.log(stream);
+
+          if (stream != null) {
+            status = "Currently Online";
+            $("#status").innerHTML = status;
+          } else if (stream = null) {
+            status = "Currently Offline";
+            $("#status").innerHTML = status;
+          }
+        },
+        error: function (errorMessage1) {
+          console.log(errorMessage1);
+        }
+     });
+
+     $.ajax ({
+        type: 'GET',
+        headers:{
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        dataType: 'jsonp',
+        url: url + users + channel,
+        success: function (data2) {
+          console.log(data2);
+          var name = data2.display_name;
+          console.log(name);
+          var logo = data2.logo;
+          $('#output').prepend('<li><img class="logo" target="_blank" src ="' + logo + '"/></li>');
+          console.log(logo);
+        },
+        error: function (errorMessage2) {
+          console.log(errorMessage2);
+        }
+      });
+    }
+  };
+
+  onLoad();
 });
